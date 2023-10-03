1c4bf344087611a82cc726937cd4705ca7a6bef0
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2023-10-03 16:17:33 +0700

    update lesson 4
    Code

--- Session 2 _ Essential C and C++ Concepts/lesson4 _ ArrayPractice/lesson4 _ ArrayPractice.cpp
+++ Session 2 _ Essential C and C++ Concepts/lesson4 _ ArrayPractice/lesson4 _ ArrayPractice.cpp
@@ -1,20 +1,45 @@
-// lesson4 _ ArrayPractice.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //int A[5] = {2, 4, 6, 8, 10}; 
+    // Can remove size of array: int A[] = {2, 4, 6, 8, 10}; 
+
+    // If the array has more initial elements than the number of allocated elements, 
+    // the remaining elements will default to 0.
+    int A[10] = {1, 2, 3};
+
+
+    /*A[0] = 12;
+    A[1] = 15;
+    A[2] = 20;
+    A[3] = 25;*/
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << A[i] << std::endl;
+    }
+
+    /*std::cout << sizeof(A) << std::endl;
+    std::cout << A[1] << std::endl;
+    printf("%d\n", A[2]);*/
+
+    // Arrays must always contain constant values, 
+    // so when we arbitrarily enter the number of arrays to declare, 
+    // it will cause an error. To be able to do this, 
+    // we must leave the initialization section empty and manually add elements one after another.
+
+    int n = 0;
+    std::cout << "Enter n = ";
+    std::cin >> n;
+    //int A[n] = { 2, 4, 6, 8 }; // Error
+    int A[n]; // It will still run with other IDEs but visual studio will give an error saying it needs to specify a specific number of arrays (const).
+    A[0] = 2;
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << A[i] << std::endl; // 2 will display and The remaining numbers will be garbage numbers
+    }
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
